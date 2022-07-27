a45fcf969ae90b8642951415cbc7dbb4eb334c91
Sercan Tuna <ssercantuna@hotmail.com>
2022-07-27 16:17:50 -0400

    done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 import java.util.concurrent.*;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // create an executor
-        ExecutorService executor; 
+        ExecutorService executor = Executors.newSingleThreadExecutor();;
 
         while (scanner.hasNext()) {
             int num = scanner.nextInt();
             // submit tasks to your executor
+            executor.submit(new PrimeLogger(num));
         }
+
+        executor.shutdown();
     }
 }
 
@@ -23,6 +28,11 @@ class PrimeLogger implements Runnable {
 
     @Override
     public void run() {
-        // print num if it is prime
+
+        boolean isPrime = IntStream.rangeClosed(2,num/2).allMatch(number -> num % number !=0);
+
+        if(isPrime){
+            System.out.println(num + " is PRIME number!");
+        }
     }
 }
